4fe1a9fbef628465451db196f30adb9daa7da61d
Benjamin <benjaminxchhakchhuak@gmail.com>
2023-11-15 22:22:23 +0530

    implementing react router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import UserDetail1 from "./Components/UserDetail1";
 import Header from "./Components/Header";
 import PostsNumber from "./Components/PostsNumber";
 import Posts from "./Components/Posts";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+      <Routes>
+        <Route exact path="/" element={<Header />} />
+      </Routes>
       <Background />
       <UserDetail1 />
-      <PostsNumber />    
+      <PostsNumber />
       <Posts />
     </>
   );
